939169460bda9dd657e08078681b74b21e742a4a
mveyone <mveyone@outlook.fr>
2023-05-31 12:20:27 +0100

    changing container port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const dbURI = "mongodb+srv://mvone:mvone123@cluster0.5agxbaq.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(80))
+  .then(result => app.listen(8080))
   .catch(err => console.log(err));
 
 // register view engine
